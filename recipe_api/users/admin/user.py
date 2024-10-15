5c57ea323790bbd9d9aad6127494fe8e3d4e96e1
amirh <amirh9476@gmail.com>
2024-10-15 20:30:10 +0330

    Add admin user

---
+++ recipe_api/users/admin/user.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from users import admin
+from users.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'),
+         {'fileds': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('important_dates', {'fields': ('last_login',)}))
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')})
+    )
+
+admin.site.register(User, UserAdmin)
