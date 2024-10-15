7ec2639d00f576c55935992726865d50b5c3d4a8
amirh <amirh9476@gmail.com>
2024-10-15 20:14:22 +0330

    Add app users and add model user

--- recipe_api/core/urls.py
+++ recipe_api/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Recipe_API project.
+URL configuration for recipe_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
