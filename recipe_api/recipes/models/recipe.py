2653aa2c39021e3e1ee8c3b4dac1c7a65a0ca64d
amirh <amirh9476@gmail.com>
2024-10-17 11:53:53 +0330

    Add models recipes app

---
+++ recipe_api/recipes/models/recipe.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from users.models import User
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    
