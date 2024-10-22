426904bb591fc8f4dfe31052eaab9cdd3f9d6da7
amirh <amirh9476@gmail.com>
2024-10-22 09:27:01 +0330

    Config pre-commit

--- recipe_api/tags/tests/test_tags_api.py
+++ recipe_api/tags/tests/test_tags_api.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.settings import DEBUG
 from recipes.models import Recipe
 from tags.models import Tag
 from tags.serialziers import TagSerializer
